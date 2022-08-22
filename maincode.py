ca1e20ced09579f3348390aff3b75f4b1d507ded
makingthefuturehappy <52582450+makingthefuturehappy@users.noreply.github.com>
2022-08-22 23:19:50 +0300

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,12 +1,13 @@
 import content_scan.latam as latam
 from datetime import date
 
-today = str(date.today().strftime("%Y/%m/%d"))
-news_sources = [] # to keep news from all web sources
+def maincode():
+    today = str(date.today().strftime("%Y/%m/%d"))
+    news_sources = [] # to keep news from all web sources
 
+    cnbc = latam.cnbc(today)
+    economist = latam.economist(today)
+    news_sources.append(cnbc, economist)
 
-cnbc = latam.cnbc(today)
-economist = latam.economist(today)
-news_sources.append(cnbc, economist)
-
-print("all done")
+    print("all done")
+    return (news_sources)
