fb801ba4df83e615d9b86ea00359167e21eb60c9
makingthefuturehappy <52582450+makingthefuturehappy@users.noreply.github.com>
2022-08-22 23:04:17 +0300

    ..

--- content_scan/latam.py
+++ content_scan/latam.py
@@ -31,3 +31,23 @@ def cnbc():
   print("The CNBC scan is done\n")
   
   return cnbc
+
+############################
+def economist():
+  print("The Economist")
+  url = "https://www.economist.com/"
+  today = str(date.today().strftime("%Y/%m/%d"))
+  economist = parser.Content(url, "The Economist", today)
+
+  # useful links selection
+  for link in economist.links_all:
+      if today in link:
+          link = url + link
+          economist.links_useful.append(link)
+      economist.links_useful_qnnty = len(economist.links_useful)
+
+  economist.get_news()
+  print("useful links qnnty:", economist.links_useful_qnnty)
+  print("The Economist scan is done\n")
+
+  return economist
--- maincode.py
+++ maincode.py
@@ -1,4 +1,9 @@
 import content_scan.latam as latam
+
+news_sources = [] # to keep news from all web sources
+
 cnbc = latam.cnbc()
+economist = latam.economist()
+news_sources.append(cnbc, economist)
+
 print("all done")
-print("1234")
