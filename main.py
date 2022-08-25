ab0b47094f54a45392c7dd5802c6291064146a69
makingthefuturehappy <52582450+makingthefuturehappy@users.noreply.github.com>
2022-08-26 00:45:18 +0300

    ..

--- main.py
+++ main.py
@@ -26,65 +26,63 @@ def main(models):
             news['text'] = text_processor.clean_text(news['text'])
 
             for model in models:
-                print(model.model_name)
+
                 try:
                     summary = model.summarize(news['text'])
                 except:
+                    print(model.model_name)
                     print("some error happened\n")
                     news.update({model.model_name: "fail"})
                     continue
 
-                print("summary:\n", summary)
                 text_processor.pretty_print(summary)
                 news.update({model.model_name: "success"})
 
-                return
-                # summary = text_processor.clean_print_update(summary)
-                    # text_processor.clean_print_update(summary)
+    return
 
                     # update model statistics
 
 
-
-    # print statistics
-    print("summarization result")
-    for source in news_sources:
-        print("source:", source.source_name)
-        print("links_all   :", len(source.links_all))
-        print("useful      :", len(source.links_useful))
-        for model in models:
-            print("\n", model.model_name)
-            fails = 0
-            success = 0
-            for news in source.news:
-                if news[model.model_name] == "fail":
-                    fails += 1
-                elif news[model.model_name] == "success":
-                    success += 1
-            print("success total:", success)
-            print("failed total:", fails)
-
-    # TG post
-
-    with open('config.yaml', 'r') as f:
-        config = yaml.safe_load(f)
-
-    creds = {"chat_id": config['tg']['chat_id'],
-             "token": config['tg']['token']}
-
-    for source in news_sources:
-        for news in source.news:
-            try:
-                tg_post = tg.format_for_tg(news['url'],
-                                        source.source_name,
-                                        news['title'],
-                                        news['summary'],
-                                        news['country'])
-                # print(tg_post)
-                # print("\n",tg_post)
-                tg.send_msg(creds, tg_post)
-            except:
-                print("failed summarization:", news['title'])
-
-    return
-    # return news_sources
+    #
+    # # print statistics
+    # print("summarization result")
+    # for source in news_sources:
+    #     print("source:", source.source_name)
+    #     print("links_all   :", len(source.links_all))
+    #     print("useful      :", len(source.links_useful))
+    #     for model in models:
+    #         print("\n", model.model_name)
+    #         fails = 0
+    #         success = 0
+    #         for news in source.news:
+    #             if news[model.model_name] == "fail":
+    #                 fails += 1
+    #             elif news[model.model_name] == "success":
+    #                 success += 1
+    #         print("success total:", success)
+    #         print("failed total:", fails)
+    #
+    # # TG post
+    #
+    # with open('config.yaml', 'r') as f:
+    #     config = yaml.safe_load(f)
+    #
+    # creds = {"chat_id": config['tg']['chat_id'],
+    #          "token": config['tg']['token']}
+    #
+    # for source in news_sources:
+    #     for news in source.news:
+    #         try:
+    #             tg_post = tg.format_for_tg(news['url'],
+    #                                     source.source_name,
+    #                                     news['title'],
+    #                                     news['summary'],
+    #                                     news['country'])
+    #             # print(tg_post)
+    #             # print("\n",tg_post)
+    #             tg.send_msg(creds, tg_post)
+    #         except:
+    #             print("failed summarization:", news['title'])
+    #
+    # return
+    # # return news_sources
